eb7df1d935612509a410ebe1fd522f424506dde8
Vishal Sharma <sharmavishal94@gmail.com>
2016-01-24 10:38:28 +0530

    Form for new entries

--- App.jsx
+++ App.jsx
@@ -1,15 +1,26 @@
 App = React.createClass({
-	getTasks(){
-		return [
-			{_id:1, text:"This is task 1"},
-			{_id:2, text:"This is task 2"},
-			{_id:3, text:"This is task 3"}
-		];
+
+	mixins: [ReactMeteorData],
+	getMeteorData(){
+		return {
+			tasks: Tasks.find({},{sort:{createdAt:-1}}).fetch()
+		}
 	},
 	renderTasks(){
-		return this.getTasks().map((task)=> {
+		return this.data.tasks.map((task)=> {
 			return <Task key={task._id} task={task} />;
 		});
+	},
+	handleSubmit(event){
+		event.preventDefault();
+		var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text: text,
+			createdAt: new Date()
+		});
+
+		React.findDOMNode(this.refs.textInput).value = "";
 	},
 	render(){
 		return (
@@ -17,6 +28,9 @@ App = React.createClass({
 				<header>
 					<h1>Todo List</h1>
 				</header>
+				<form className="new-entry" onSubmit={this.handleSubmit}>
+					<input type="text" ref="textInput" placeholder="Enter new entry" />
+				</form>			
 				<ul>
 					{this.renderTasks()}
 				</ul>
